0bbe56e3b12a57d4b88b5c78d26a8d148d5a84a0
Sanjay Bankar <sanjaybankar12@gmail.com>
2018-08-18 19:08:57 +0530

    add firebase property to prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment={
-    production:true
+    production:true,
+    firebaseConfig:{
+        apiKey: "AIzaSyBMbjbgIehpgeHmFJqvqS8rpQGvo7qqbYQ",
+        authDomain: "anguarcrud.firebaseapp.com",
+        databaseURL: "https://anguarcrud.firebaseio.com",
+        projectId: "anguarcrud",
+        storageBucket: "anguarcrud.appspot.com",
+        messagingSenderId: "634395509269"
+    }
 }
